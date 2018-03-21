41ee56304ce090c3ab711e4d0a934f3ecbe7b0af
SunYanhui <sunyanhui@xxkuaipao.com>
2018-03-20 23:35:44 -0700

    add questions

---
+++ app/questions/questions.js
@@ -0,0 +1,46 @@
+Page({
+  data: {
+    questions: [
+      {
+        question: 'Hello world <3',
+        answer: '2017.3.26'
+      },
+      {
+        question: '奶瓶品牌？',
+        answer: 'NUK'
+      },
+      {
+        question: '奶粉？',
+        answer: '喜乐'
+      },
+      {
+        question: '出生医院？',
+        answer: '北京天坛医院'
+      },
+      {
+        question: '害怕？',
+        answer: '扫地机器人'
+      },
+      {
+        question: '喜欢？',
+        answer: '吃尽'
+      },
+      {
+        question: '最近看的书？',
+        answer: 'Brown bear'
+      },
+      {
+        question: '最喜欢的歌？',
+        answer: 'Ponytail'
+      },
+      {
+        question: '学习？',
+        answer: 'Ready Go'
+      },
+      {
+        question: '走路咋样？',
+        answer: '着急的时候还是喜欢爬'
+      }
+    ]
+  }
+})
