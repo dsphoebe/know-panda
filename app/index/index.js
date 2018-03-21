bafc615b611ebbe25c0846cad62e30b6030e3d2d
SunYanhui <sunyanhui@xxkuaipao.com>
2018-03-20 23:35:35 -0700

    remove request code

--- app/index/index.js
+++ app/index/index.js
@@ -3,17 +3,6 @@ Page({
     userInfo: {}
   },
   onLoad() {
-    const uid = wx.getStorage({key: 'uid'})
-
-    if (!uid) {
-      wx.navigateBack()
-    }
-
-    wx.request({
-      url: `/users/${uid}`
-    })
-      .then(res => {
-        
-      })
+    
   }
 })
